42b961ac413a067c910220ec97ef4c56bb5ab3f4
kaylawessling <kwessling05@gmail.com>
2019-03-27 16:34:58 -0400

    began styling header using Routes

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import Home from './Home';
-import EasyGame from "./EasyGame";
-import HardGame from './HardGame';
-import { withRouter } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
-// const Header = () => (
+const Header = () => (
   <header>
     <ul className="main-nav">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/EasyGame">Easy Game</Link></li>
-      <li><Link to="/HardGame">Hard Game</Link></li>
-      <li><Link to="/GameOver">Restart</Link></li>
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink to="/EasyGame">Easy Game</NavLink></li>
+      <li><NavLink to="/HardGame">Hard Game</NavLink></li>
+      <li><NavLink to="/GameOver">Restart</NavLink></li>
     </ul>
+  </header>
 )
 
 
-const Header = ({ restartGame }) => (
-  <div className="grid-header-container">
-    <div className="justify-end">
-      <button onClick={restartGame} className="restart-button">Restart Game</button>
-    </div>
-  </div>
-);
+// const Header = ({ restartGame }) => (
+//   <div className="grid-header-container">
+//     <div className="justify-end">
+//       <button onClick={restartGame} className="restart-button">Restart Game</button>
+//     </div>
+//   </div>
+// );
 
 export default Header;
