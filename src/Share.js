17ed0228d4039309f862a8eead1c695048f59964
kaylawessling <kwessling05@gmail.com>
2019-03-28 13:40:43 -0400

    added api/share to Facebook functionality to Share.js

--- src/Share.js
+++ src/Share.js
@@ -1,1 +1,23 @@
 import React from 'react';
+import Header from 'header';
+
+<h1>Enjoy the game?</h1>
+
+<h3>Share it on Facebook!</h3>
+
+<div id="shareBtn" class="btn btn-success clearfix">Share</div>
+
+<h3 style="margin-top: 50px">
+  <hr />
+  <a class="btn btn-small"  href="https://developers.facebook.com/docs/sharing/reference/share-dialog">Share Dialog Documentation</a>
+</h3>
+
+<script>
+document.getElementById('shareBtn').onclick = function() {
+  FB.ui({
+    method: 'share',
+    display: 'popup',
+    href: 'https://developers.facebook.com/docs/',
+  }, function(response){});
+}
+</script>
