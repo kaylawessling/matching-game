2bbbbcd013d784bc23fabb7de68a6024503101cd
kaylawessling <kwessling05@gmail.com>
2019-03-28 09:46:19 -0400

    removed Home.js and EasyGame.js, updated code

--- src/GameOver.js
+++ src/GameOver.js
@@ -3,8 +3,7 @@ import React from 'react';
 const GameOver = ({ restartGame }) => (
   <div className="justify-center">
     <h1>Game Over!</h1>
-    <h3>If you enjoyed playing this game, share it on Facebook!</h3>
-    <button className="facebook-button" onClick={restartGame}>Share it on Facebook!</button>
+    <h3>If you enjoyed playing this game, follow me @iamkenec for more...</h3>
     <button className="restart-button" onClick={restartGame}>Restart Game</button>
   </div>
 );
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import { NavLink } from "react-router-dom"
 
-const Header = () => (
-  <header>
-    <ul className="main-nav">
-      <li><NavLink exact to="/">Home</NavLink></li>
-      <li><NavLink to="/EasyGame">Easy Game</NavLink></li>
-      <li><NavLink to="/HardGame">Hard Game</NavLink></li>
-      <li><NavLink to="/GameOver">Restart</NavLink></li>
-    </ul>
-  </header>
-)
-
-
-// const Header = ({ restartGame }) => (
-//   <div className="grid-header-container">
-//     <div className="justify-end">
-//       <button onClick={restartGame} className="restart-button">Restart Game</button>
-//     </div>
-//   </div>
-// );
+const Header = ({ restartGame }) => (
+  <div className="grid-header-container">
+    <div className="justify-left timer"></div>
+    <div className="justify-center game-status-text"></div>
+    <div className="justify-end">
+      <button onClick={restartGame} className="restart-button">Restart Game</button>
+    </div>
+  </div>
+);
 
 export default Header;
