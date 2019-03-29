5d1fc0c3027bb429acf74d8f743fe80490033b56
kaylawessling <kwessling05@gmail.com>
2019-03-29 11:30:30 -0400

    most recent iteration of Share.js

--- src/Share.js
+++ src/Share.js
@@ -8,13 +8,13 @@ constructor () {
 }
 
   componentDidMount() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?zip=40205,us&APIID={46e0a8caad1359cdf98ff659d53ab742}")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Louisville&APIID=46e0a8caad1359cdf98ff659d53ab742")
       .then(res => res.json())
       .then(
         (result) => {
           console.log('api returned', result)
           this.setState({
-            weather: result.data.image_original_url
+            weather: result.data.Louisville
           })
         })
       }
